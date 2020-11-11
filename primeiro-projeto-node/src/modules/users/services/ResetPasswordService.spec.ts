b0dc861858c37af7ceafcfca9f7c0790ef2311e8
Murilo Baptista <muboeira@gmail.com>
2020-11-11 08:11:42 -0300

    .

--- primeiro-projeto-node/src/modules/users/services/ResetPasswordService.spec.ts
+++ primeiro-projeto-node/src/modules/users/services/ResetPasswordService.spec.ts
@@ -1,7 +1,5 @@
 import AppError from '@shared/errors/AppError';
 
-import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
-
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeUserTokensRepository from '../repositories/fakes/FakeUserTokensRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
